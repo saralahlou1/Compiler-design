77acb7e105426fe6e8eb223ee80332139ddf5a83
Sara Lahlou <sl@Saras-MacBook-Air.local>
2024-02-09 17:35:36 -0500

    added missing cases in switch for sementics

--- src/java/sem/NameAnalyzer.java
+++ src/java/sem/NameAnalyzer.java
@@ -38,7 +38,45 @@ public class NameAnalyzer extends BaseSemanticAnalyzer {
 			case Type t -> {}
 
 			// to complete ...
-		};
+            case AddressOfExpr addressOfExpr -> {
+            }
+            case ArrayAccessExpr arrayAccessExpr -> {
+            }
+            case Assign assign -> {
+            }
+            case BinOp binOp -> {
+            }
+            case Break aBreak -> {
+            }
+            case ChrLiteral chrLiteral -> {
+            }
+            case Continue aContinue -> {
+            }
+            case ExprStmt exprStmt -> {
+            }
+            case FieldAccessExpr fieldAccessExpr -> {
+            }
+            case FunCallExpr funCallExpr -> {
+            }
+            case FunProto funProto -> {
+            }
+            case If anIf -> {
+            }
+            case IntLiteral intLiteral -> {
+            }
+            case Return aReturn -> {
+            }
+            case SizeOfExpr sizeOfExpr -> {
+            }
+            case StrLiteral strLiteral -> {
+            }
+            case TypecastExpr typecastExpr -> {
+            }
+            case ValueAtExpr valueAtExpr -> {
+            }
+            case While aWhile -> {
+            }
+        };
 
 	}
 
--- src/java/sem/TypeAnalyzer.java
+++ src/java/sem/TypeAnalyzer.java
@@ -46,7 +46,26 @@ public class TypeAnalyzer extends BaseSemanticAnalyzer {
 			}
 
 			// to complete ...
-		};
+            case AddressOfExpr addressOfExpr -> null;
+            case ArrayAccessExpr arrayAccessExpr -> null;
+            case Assign assign -> null;
+            case BinOp binOp -> null;
+            case Break aBreak -> null;
+            case ChrLiteral chrLiteral -> null;
+            case Continue aContinue -> null;
+            case ExprStmt exprStmt -> null;
+            case FieldAccessExpr fieldAccessExpr -> null;
+            case FunCallExpr funCallExpr -> null;
+            case FunProto funProto -> null;
+            case If anIf -> null;
+            case IntLiteral intLiteral -> null;
+            case Return aReturn -> null;
+            case SizeOfExpr sizeOfExpr -> null;
+            case StrLiteral strLiteral -> null;
+            case TypecastExpr typecastExpr -> null;
+            case ValueAtExpr valueAtExpr -> null;
+            case While aWhile -> null;
+        };
 
 	}
 
