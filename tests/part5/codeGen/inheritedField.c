177b06362f0f814aedad48dd39bbef5127404015
Sara Lahlou <sl@Saras-MacBook-Air.local>
2024-04-10 18:39:50 -0400

    added test where field is inherited

---
+++ tests/part5/codeGen/inheritedField.c
@@ -0,0 +1,14 @@
+class Course{
+    int x;
+    void whereToAttend(){x = 0;}
+}
+class ExtendedCourse extends Course {
+    int y;
+}
+
+void main() {
+    class ExtendedCourse course;
+    course = new class ExtendedCourse();
+    course.x = 51;
+    print_i(course.x);
+}
